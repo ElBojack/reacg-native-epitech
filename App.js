ada197cb0bb0ea79586d996323c0bcc1ad9b18ce
enzo <enzo.nivert@epitech.eu>
2021-11-05 09:47:52 +0100

    expo and linter

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+StatusBar.setHidden(true);
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ecf0f1",
+  },
+  image: {
+    width: 200,
+    height: 150,
+  },
+  header: {
+    paddingVertical: 25,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+  },
+  headerText: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
+  contentContainer: {
+    alignItems: "center",
+  },
+  footer: {
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingVertical: 15,
+  },
+  button: {
+    padding: 5,
+  },
+  textStyle: {
+    fontSize: 20,
+    textDecorationLine: "line-through",
   },
 });
+
+const App = () => {
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+  const onPress = () => {
+    setData([{ value, selected: false }, ...data]);
+  };
+  const onPressText = (index) => {
+    const selectedItem = [...data];
+    selectedItem[index].selected = !selectedItem[index].selected;
+    setData(selectedItem);
+  };
+  return (
+    <View style={styles.root}>
+      <View style={styles.header}>
+        <TextInput
+          style={styles.headerText}
+          onChangeText={setValue}
+          value={value}
+          placeholder="Enter a destination"
+        />
+        <TouchableOpacity onPress={onPress} style={styles.button}>
+          <Text>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView
+        style={styles.content}
+        contentContainerStyle={styles.contentContainer}
+      >
+        {data.map(({ value, selected }, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => onPressText(index)}
+            style={styles.button}
+          >
+            <Text
+              style={[
+                styles.textStyle,
+                { textDecorationLine: selected ? "line-through" : "none" },
+              ]}
+            >
+              {value}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
+    </View>
+  );
+};
+
+export default App;
+
